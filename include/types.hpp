3074f30b845fcfe4ec104b3c665d14fd34967a83
isanbaev <a.isanbaev@prosoftsystems.ru>
2025-10-08 18:30:45 +0500

    add: fixed string class and tests

--- include/types.hpp
+++ include/types.hpp
@@ -1,26 +1,54 @@
 #pragma once
+#include <cstddef>
+#include <tuple>
 
 namespace stdx::details {
 
 // Шаблонный класс, хранящий C-style строку фиксированной длины
-
-// ваш код здесь
+template <std::size_t N>
 struct fixed_string {
-    // ваш код здесь
+    char data[N] = {};
+
+    constexpr fixed_string(const char (&str)[N]) {
+        for (std::size_t i = 0; i < N; ++i) {
+            data[i] = str[i];
+        }
+    }
+
+    template <std::size_t M>
+    constexpr fixed_string(const char (&str)[M]) 
+        requires (M <= N) 
+    {
+        for (std::size_t i = 0; i < M - 1; ++i) {
+            data[i] = str[i];
+        }
+    }
+
+    constexpr fixed_string(const char* begin, const char* end) {
+        std::size_t i = 0;
+        while (begin != end && i < N) {
+            data[i++] = *begin++;
+        }
+    }
 };
 
-// Шаблонный класс, хранящий fixed_string достаточной длины для хранения ошибки парсинга
 
-// ваш код здесь
-struct parse_error {};
+// Шаблонный класс, хранящий fixed_string достаточной длины для хранения ошибки парсинга
+struct parse_error : fixed_string<41> {
+    template <std::size_t M>
+    constexpr parse_error(const char (&str)[M]) : fixed_string<41>(str) {}
+};
 
 // Шаблонный класс для хранения результатов парсинга
-
 template <typename... Ts>
 struct scan_result {
-// ваш код здесь
-// измените реализацию
-    int i;
+    std::tuple<Ts...> data;
+
+    constexpr scan_result(Ts... args) : data(args...) {}
+
+    constexpr const std::tuple<Ts...>& values() const {
+        return data;
+    }
 };
 
 } // namespace stdx::details
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,33 @@
-#include "scan.hpp"
+#include "types.hpp"
+
+// ========== Тестирование класса fixed_string ==========
+// === 1. Проверка создания из строкового литерала ===
+static_assert(stdx::details::fixed_string{"hello"}.data[0] == 'h');
+static_assert(stdx::details::fixed_string{"hello"}.data[4] == 'o');
+static_assert(stdx::details::fixed_string{"hello"}.data[5] == '\0');
+
+// === 2. Проверка размера ===
+static_assert(sizeof(stdx::details::fixed_string{"hi"}) == sizeof(char[3]));
+
+// === 3. Проверка конструктора от массива меньшего размера ===
+static_assert(stdx::details::fixed_string<10>{"hi"}.data[2] == '\0');
+static_assert(stdx::details::fixed_string<10>{"hi"}.data[9] == '\0');
+
+// строка "ab" (длина 2 + 1 = 3) не помещается в fixed_string<2>
+// static_assert(stdx::details::fixed_string<2>{"ab"}); // ошибка компиляции
+
+// === 4. Проверка конструктора от указателей ===
+constexpr const char* str = "Hello world!";
+constexpr auto fs = stdx::details::fixed_string<15>(str, str + 4);
+static_assert(fs.data[0] == 'H');
+static_assert(fs.data[3] == 'l');
+static_assert(fs.data[4] == '\0');
+
+constexpr stdx::details::fixed_string<3> str3(str, str);
+static_assert(str3.data[0] == '\0');
+static_assert(str3.data[1] == '\0');
+static_assert(str3.data[2] == '\0');
 
 int main() { 
-    static_assert(stdx::scan<stdx::details::format_string{}, stdx::details::fixed_string{}, int>().i == 42); 
+    return 0;
 }
